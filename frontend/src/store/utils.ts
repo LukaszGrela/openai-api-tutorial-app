d1c145f5bb3726459fe75dd02cb2690d26c78607
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2023-12-27 20:53:19 +0100

    toError helper

--- frontend/src/store/utils.ts
+++ frontend/src/store/utils.ts
@@ -1,8 +1,18 @@
+import type { THistoryResponse } from '../api/types';
 import { hasOwn } from '../utils/object/hasOwn';
-import type { THistoryResponse } from './types';
+import { TError } from './types';
 
 export const guardTHistoryResponse = (
   test: unknown
 ): test is THistoryResponse => {
   return !!test && hasOwn(test, 'list') && hasOwn(test, 'rateLimit');
 };
+
+export const toError = (error: unknown): TError => ({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  name: (error as Error).name || (error as any).type,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  code: (error as any).code,
+  message: (error as Error).message,
+  stack: (error as Error).stack,
+});
