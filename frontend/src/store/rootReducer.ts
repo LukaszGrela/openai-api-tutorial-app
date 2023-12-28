85cd219f6b3aece7d9dc24416ce49f96c17e7f33
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2023-12-29 00:15:06 +0100

    add route slice to reducer

--- frontend/src/store/rootReducer.ts
+++ frontend/src/store/rootReducer.ts
@@ -3,12 +3,14 @@ import { historySlice } from './slice/history';
 import { chatSlice } from './slice/chat';
 import { chatConfigurationSlice } from './slice/chat-configuration';
 import { chatLimitsSlice } from './slice/chat-limits';
+import { routeSlice } from './slice/route';
 
 const rootReducer = combineReducers({
   chat: chatSlice,
   chatConfiguration: chatConfigurationSlice,
   chatLimits: chatLimitsSlice,
   history: historySlice,
+  route: routeSlice,
 });
 
 export default rootReducer;
